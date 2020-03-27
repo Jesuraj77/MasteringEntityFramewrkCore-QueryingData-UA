695605e86c47b656d05bf5df9ecf0444fe3c3f29
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:24:40 +0530

    files added

---
+++ BusinessLayer/Services/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+   public  class Class1
+    {
+        //Use aggregate functions to compute min, max, average, sum, and count on the table. Use group by clause to group the records.
+
+
+      
+
+
+    }
+}
